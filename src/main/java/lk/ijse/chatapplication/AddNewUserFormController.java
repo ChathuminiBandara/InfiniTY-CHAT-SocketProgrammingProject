375bc0c4097af9f077b94a6d2f988929342d7333
bandara <chathubandara500@gmail.com>
2024-02-01 01:20:32 +0530

    Modified The UI

--- src/main/java/lk/ijse/chatapplication/AddNewUserFormController.java
+++ src/main/java/lk/ijse/chatapplication/AddNewUserFormController.java
@@ -1,20 +1,29 @@
 package lk.ijse.chatapplication;
 
-import com.jfoenix.controls.JFXTextField;
+
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.TextField;
 import javafx.stage.Stage;
+import lk.ijse.chatapplication.Model.UserModel;
+import lk.ijse.chatapplication.dto.UserDto;
 
 import java.io.IOException;
+import java.sql.SQLException;
 
 public class AddNewUserFormController extends Application {
-    public JFXTextField txtUsername;
+    public TextField txtUsername;
     static String username;
+    public TextField password;
+
+    private UserModel userModel = new UserModel();
+
     @Override
     public void start(Stage stage) throws Exception {
-        stage.setScene(new Scene(FXMLLoader.load(getClass().getResource("/View/AddNewUserForm.fxml"))));
+        stage.setScene(new Scene(FXMLLoader.load(getClass().getResource("/View/NewUser.fxml"))));
         stage.show();
 
         new Thread(()->{
@@ -32,7 +41,26 @@ public class AddNewUserFormController extends Application {
         Stage stage = new Stage();
         stage.setScene(new Scene(FXMLLoader.load(getClass().getResource("/View/ClientForm.fxml"))));
         stage.setTitle(txtUsername.getText() + "'s Chat");
+        savepassword();
         stage.show();
         txtUsername.clear();
+        password.clear();
+    }
+    public void savepassword(){
+         String pw = password.getText();
+         String username1 = txtUsername.getText();
+
+        var dto = new UserDto(pw,username1);
+
+        try {
+            boolean isSaved = userModel.saveUser(dto);
+
+            if (isSaved) {
+                new Alert(Alert.AlertType.CONFIRMATION, "User saved!").show();
+
+            }
+        } catch (SQLException e) {
+            new Alert(Alert.AlertType.ERROR, e.getMessage()).show();
+        }
     }
 }
